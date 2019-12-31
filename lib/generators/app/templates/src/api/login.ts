779f3860078ffeb42f17e83cf20b7d5a50883687
wangyuan0108 <870147852@qq.com>
2019-12-31 15:38:35 +0800

    分别引入不同的请求库

--- lib/generators/app/templates/src/api/login.ts
+++ lib/generators/app/templates/src/api/login.ts
@@ -1,10 +1,30 @@
-import request from '@/services/request';
-import { ILoginParams } from '@/typings/login';
+<% if (isAxios) { %> import {fetch, post} from '@/services/axiosRequest.js';<% }else { %> import request from '@/services/request'; <%}%>
+<% if (isAxios){ %>
+export const login = (conf)=>(cb)=> {
+  return fetch({
+    method: 'post',
+    url: '/api/login', //接口名称
+    params:conf,
+  })(cb);
+}
 
-export function login(data: ILoginParams) {
+/**
+ * 添加
+ */
+const add = (conf) => (cb) => (errCb) => {
+  return post({
+      method: 'post',
+      url: 'advertisement/add.html',
+      data: conf
+  })(cb)(errCb);
+};
+<% } else { %>
+export function login(data) {
   return request({
     url: '/api/login', //接口名称
     method: 'post',
     data,
   });
 }
+<% } %>
+
